3e8b2d67ed95883ea222c5ab737a6aea02d0d993
jpomfret <jpomfret7@gmail.com>
2022-05-13 17:20:32 +0100

    function

---
+++ FilesToCosmosDb/run.ps1
@@ -0,0 +1,12 @@
+# Input bindings are passed in via param block.
+param([byte[]] $InputBlob, $TriggerMetadata)
+
+# Write out the blob name and size to the information log.
+Write-Host "PowerShell Blob trigger function Processed blob! Name: $($TriggerMetadata.Name) Size: $($InputBlob.Length) bytes"
+
+
+Push-OutputBinding -Name outputDocument -Value @{ 
+    date     = Get-Date
+    fileName = $TriggerMetadata.Name
+    fileSize = $($InputBlob.Length)
+} 
