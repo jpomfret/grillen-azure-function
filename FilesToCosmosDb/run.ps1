922665d5b58a9ea28ac9f4d3b3a062883142a8aa
jpomfret <jpomfret7@gmail.com>
2022-05-26 18:32:14 +0100

    remove double space

--- FilesToCosmosDb/run.ps1
+++ FilesToCosmosDb/run.ps1
@@ -4,7 +4,6 @@ param([byte[]] $InputBlob, $TriggerMetadata)
 # Write out the blob name and size to the information log.
 Write-Host "PowerShell Blob trigger function Processed blob! Name: $($TriggerMetadata.Name) Size: $($InputBlob.Length) bytes"
 
-
 Push-OutputBinding -Name outputDocument -Value @{ 
     date     = Get-Date
     fileName = $TriggerMetadata.Name
